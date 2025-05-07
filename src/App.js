1bb93f25c721ddaa6696174f1791050a9228428d
Marwan Mohamed <lorallords@gmail.com>
2025-05-07 23:33:06 +0300

    Hashrouter instead of Browserrouter

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
 export default App;*/
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
 import StateCouncilPage from "./Components/StateCouncilPage/StateCouncilPage";
@@ -19,7 +19,7 @@ import NewCaseForm from "./Components/NewCaseForm/NewCaseForm";
 
 function App() {
   return (
-    <Router basename="BackEnd-GradePorject">
+    <Router>
       <Routes>
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
